7665577b291383c4fbb074f5be8c87d6a897d7bd
Juninho <jonildopereira5@gmail.com>
2024-11-01 21:03:04 -0300

    Melhorando o código

--- ListaSequencial.java
+++ ListaSequencial.java
@@ -2,31 +2,31 @@ import java.util.Arrays;
 
 public class ListaSequencial {
     private Livro[] livros;
-    private int tamanho;
+    private int quantidade;
 
     //construtor para inicializar a lista com capacidade inicial
     public ListaSequencial(int capacidadeInicial){
         livros = new Livro[capacidadeInicial];
-        tamanho = 0;
+        quantidade = 0;
     }
 
     //Método para adicionar um novo elemento no final
     public void add(Livro livro){
-        if (tamanho == livros.length) {
+        if (quantidade == livros.length) {
             //Redimensionar o array de objetos quando atingir a capacidade
             livros = Arrays.copyOf(livros, livros.length * 2);
         }
-        livros[tamanho] = livro;
-        tamanho++;
+        livros[quantidade] = livro;
+        quantidade++;
     }
 
     //Método para listar todos o livros
     public void listBooks(){
-        if (tamanho == 0) {
+        if (quantidade == 0) {
             System.out.println("Lista vazia");
         }
         else{
-            for(int i = 0; i < tamanho; i++){
+            for(int i = 0; i < quantidade; i++){
                 System.out.println(livros[i]);
             }
         }
@@ -34,8 +34,8 @@ public class ListaSequencial {
 
     //Método para ordenar livros
     public void sortBooks(){
-        for (int i = 0; i < tamanho - 1; i++) {
-            for (int j = i + 1; j < tamanho; j++) {
+        for (int i = 0; i < quantidade - 1; i++) {
+            for (int j = i + 1; j < quantidade; j++) {
                 if (livros[i].titulo.compareTo(livros[j].titulo) > 0) {
                     Livro temp = livros[i];
                     livros[i] = livros[j];
@@ -47,9 +47,9 @@ public class ListaSequencial {
     }
 
     //Método para buscar livro por título
-    public Livro searchBookByTitle(String tituto){
-        for(int i = 0; i < tamanho; i++){
-            if (livros[i].titulo.equalsIgnoreCase(tituto)) {
+    public Livro searchBookByTitle(String titulo){
+        for(int i = 0; i < quantidade; i++){
+            if (livros[i].titulo.equalsIgnoreCase(titulo)) {
                 return livros[i];
             }
         }
